98f8ef76c638cc006388ac99a536d1f49ea81429
Lee <blackmonkey9256@gmail.com>
2018-11-12 13:05:13 +0900

    List with JSON

--- lib/main.dart
+++ lib/main.dart
@@ -31,28 +31,86 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  String url = 'https://randomuser.me/api/';
+  String url = 'https://randomuser.me/api?results=15';
+  List data;
 
   Future<String> makeRequest() async {
     var response = await http.get(Uri.encodeFull(url),
         headers: {"Accept": "application/json"});
 
-    List data;
 
-    var extractData = json.decode(response.body);
-    data = extractData["results"];
+    setState(() {
+      var extractData = json.decode(response.body);
+      data = extractData["results"];
+    });
 
-    print(data[0]["name"]["first"]);
+
+
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.makeRequest();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Contact List'),
+      ),//AppBar
+      body: new ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, i){
+          return new ListTile(
+            title: new Text(data[i]["name"]["first"]),
+            subtitle: new Text(data[i]["phone"]),
+            leading: new CircleAvatar(
+              backgroundImage: new NetworkImage(data[i]["picture"]["thumbnail"]),
+            ),//CircleAvatar
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      new SecondPage(data[i])
+                ));//MaterialPageRoute
+            },
+          );//ListTilet
+        }),//ListView.builder
+    );//Scaffold
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  SecondPage(this.data);
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Second page'),
+      ),//AppBar
       body: new Center(
-        child: new RaisedButton(
-          child: new Text('Make Request'),
-          onPressed: makeRequest,
-        ),//RaiseButton,
+        child: new Container(
+          width: 150.0,
+          height: 150.0,
+          decoration: new BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: new DecorationImage(
+              image: new NetworkImage(data["picture"]["large"]),
+              fit: BoxFit.cover,
+            ),//DecorationImage
+            borderRadius: new BorderRadius.all(new Radius.circular(75.0)),
+            border: new Border.all(
+              color: Colors.greenAccent,
+              width: 4.0
+            ),//Border.all
+          ),//BoxDecoration
+        ),//Container
       ),//Center
     );//Scaffold
   }
